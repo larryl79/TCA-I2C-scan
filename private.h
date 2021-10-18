b90b2c80f70bdeaf6390eaeb2b29f4409c5d539b
larryl79 <larryl79@gmail.com>
2021-10-18 21:36:41 +0100

    Create private.h
    Private definitions.
    Change definitions as you need.

---
+++ private.h
@@ -0,0 +1,6 @@
+/* This definitons only used by wifi and ota. cahnge it as you need it */
+#define OTA_HOST          "CHANGE-YOUR-OTA-HOSTNAME"           // OTA Hostname
+#define CLIENTSSID        "CHANGE-YOUR-WIFI-SSID"              // wi-fi netwrok name
+#define CLIENTPASSWORD    "CHANGE-YOUR-WIFI-PASSWORD"          // wi-fi network password
+#define AP_SSID           "I2CMUX-Scan"                        // Standalone AP SSID
+#define AP_PASS           "Magicword12"                        // Standalone AP password
